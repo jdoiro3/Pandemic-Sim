8cd2dd6b922ca44831347fee5f4b310a9703a90d
Joseph Doiron <josephdoiron1234@yahoo.com>
2020-09-01 08:13:18 -0400

    adjusted sliders and added another hub

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,31 @@
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var c = new Circle(canvas.width/2, canvas.height/2, 450);
+var c = new Circle(canvas.width/4, canvas.height/4, 200);
+var c2 = new Circle((canvas.width-canvas.width/4), (canvas.height-canvas.height/4), 200);
 
 var people = [];
 for (let i = 0; i < 1000; i++) {
-	let pnt = c.random();
-	let person = new Person(pnt.x, pnt.y, 's');
-	people.push(person);
+	if (Math.random() > .5) {
+		let pnt = c.random();
+		let person = new Person(pnt.x, pnt.y, 's');
+		people.push(person);
+	} else {
+		let pnt = c2.random();
+		let person = new Person(pnt.x, pnt.y, 's');
+		people.push(person);
+	}
 }
 
-function move(person, step_size, qt) {
-	if (Math.random() < .0001) {
-		let randp = c.random();
-		person.toPoint(randp);
+function move(person, step_size, prob_to_travel, qt) {
+	if (Math.random() < prob_to_travel) {
+		if (Math.random() > .5) {
+			let randp = c.random();
+			person.toPoint(randp);
+		} else {
+			let randp = c2.random();
+			person.toPoint(randp);
+		}
 	}
 
 	person.update(step_size);
@@ -22,14 +34,16 @@ function move(person, step_size, qt) {
 
 function refresh() {
 
-	let social_dist_v = document.getElementById("social-dist").value;
+	let social_dist = document.getElementById("social-dist");
+	let social_dis_max = social_dist.max;
+	let social_dist_v = social_dist.value;
 	let travel_v = document.getElementById("travel").value;
 	let inf_v = document.getElementById("inf").value;
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 	ctx.fillStyle = "rgb(0,0,0)";
+	ctx.strokeStyle = "rgb(255, 255, 255)";
 	ctx.fillRect(0,0, canvas.width, canvas.height);
-	c.draw(ctx);
 
 	let q = new QuadTree(
 		new Rect(canvas.width/2, canvas.height/2, canvas.width, canvas.height),
@@ -45,7 +59,8 @@ function refresh() {
 	//q.show(ctx);
 
 	people.forEach(p => {
-		move(p, social_dist_v, q);
+		let step_size = Math.abs(social_dist_v - social_dis_max);
+		move(p, step_size, travel_v, q);
 		if (p.status === 'i') {
 			let exposed = p.getExposed(q);
 			exposed.forEach(p => p.expose());
--- scripts/utils.js
+++ scripts/utils.js
@@ -143,7 +143,6 @@ class Rect {
     }
 
     draw(ctx) {
-        ctx.strokeStyle = "rgb(255, 255, 255)";
         ctx.strokeRect(this.topLeft.x, this.topLeft.y, this.w, this.h);
     }
 
